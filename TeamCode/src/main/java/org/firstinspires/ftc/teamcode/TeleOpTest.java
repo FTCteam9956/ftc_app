47054c53d67843a8dcecdbffb48c2d325e7a7d4b
s1a1m <sam.christensen73@gmail.com>
2017-10-08 13:51:32 -0500

    TeleOpTest updated.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -1,11 +1,15 @@
-package org.firstinspires.ftc.teamcode;
+//TeleOpTest.java
+//Goal of this program is just to spin 1 motor named left1.
 
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+//This is where we define what the op mode will be called on the FTC app.
 @TeleOp(name = "TeleOpTest", group = "Teleop")
+
 public class TeleOpTest extends LinearOpMode
 {
     DcMotor left1;
@@ -13,9 +17,10 @@ public class TeleOpTest extends LinearOpMode
     @Override
     public void runOpMode()
     {
+        //Remember to label the motors the same as how we name them in the configuration on the FTC app. - Sam C.
         left1 = hardwareMap.dcMotor.get("left1");
         left1.setDirection(DcMotorSimple.Direction.REVERSE);
-        left1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
@@ -23,6 +28,9 @@ public class TeleOpTest extends LinearOpMode
         {
             left1.setPower(gamepad1.left_stick_y);
             idle();
+
+            telemetry.addData("ENCODER: ", left1.getCurrentPosition());
+            telemetry.update();
         }
     }
 }
